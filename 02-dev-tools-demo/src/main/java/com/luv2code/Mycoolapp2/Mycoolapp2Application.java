a6cdde987aa46037cabdc3b3384baafcaa663f55
Muhammed Mustafa Balçiçek <balcicekmustafa46@gmail.com>
2024-03-05 21:30:00 +0300

    JAVA Spring Boot Basic Learning
    Steps of my learning process

---
+++ 02-dev-tools-demo/src/main/java/com/luv2code/Mycoolapp2/Mycoolapp2Application.java
@@ -0,0 +1,12 @@
+package com.luv2code.Mycoolapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mycoolapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mycoolapp2Application.class, args);
+	}
+}
---
+++ 02-dev-tools-demo/src/test/java/com/luv2code/Mycoolapp2/Mycoolapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.Mycoolapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycoolapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
